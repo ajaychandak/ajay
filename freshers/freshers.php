2b5f2e107fcb1c9cdfe443964671829b994fc135
Ajay Chandak <ajaychandak2092@gmail.com>
2020-02-08 00:57:10 +0530

    Freshers Page First Look

---
+++ freshers/freshers.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FRESHERS</title>
+        <link rel="stylesheet" href="css/freshers.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Concert+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!-- The video -->
+        <div class="section">
+            <h1>KRONOS</h1>
+            <div class="video-container">
+                <div class="color-overlay"></div>
+                <video autoplay muted loop id="myVideo" style="object-fit: fill ;width: 100%;">
+                <source src="bckgnd.mp4" type="video/mp4">
+                </video>
+            </div>    
+        </div>
+        <div class="topbar">
+                <div class="home">HOME</div>
+                <div class="about">ABOUT</div>
+                <div class="events">EVENTS</div>
+                <div class="leaderboard">LEADERBOARD</div>
+                <div class="kys">KYS</div>
+                <div class="questionnaire">QUESTIONNAIRE</div>                
+        </div>    
+    </body>
+</html>        
